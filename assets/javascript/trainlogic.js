928800e6da33a0edd3765b6a624cd591f73e4672
Sean Hagan <sphagan1088@yahoo.com>
2017-11-17 23:05:04 -0800

    added console log and alert to js file

--- assets/javascript/trainlogic.js
+++ assets/javascript/trainlogic.js
@@ -6,17 +6,40 @@ var config = {
     projectId: "train-hw-5e478",
     storageBucket: "",
     messagingSenderId: "261426075834"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  //Add Train Button
-  $("add-train-btn").on("click", function(event) {
-      event.preventDefault();
-      //Grab user input
-      var trainName = $("#train-name-input").val().trim();
-      var destination = $("#destination-input").val().trim();
-      var firstTrainTime = $("#first-train-time-input").val().trim();
-      var frequency = $("#frequency-input").val().trim();
-  })
+//Add Train Button
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+    //Grab user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainTime = $("#first-train-time-input").val().trim();
+    var trainFrequency = $("#frequency-input").val().trim();
+
+    //Create local temporary object for holding train data
+    var newTrain = {
+        name: trainName,
+        dest: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
+    };
+
+    //Upload train data to the database 
+    database.ref().push(newTrain);
+    //Log in console
+    console.log(newTrain.name);
+    console.log(newTrain.dest);
+    console.log(newTrain.time);
+    console.log(newTrain.frequency);
+    //Alert
+    alert("Train sucessfully added");
+    //Clear all text boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-time-input").val("");
+    $("#frequency-input").val("");
+});
